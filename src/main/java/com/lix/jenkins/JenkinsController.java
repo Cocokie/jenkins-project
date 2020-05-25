71ddb07455912f7d3d7d22ef19b79a458ef2a351
lixin <1254759600@qq.com>
2020-05-25 09:49:29 +0800

    firstCommit

---
+++ src/main/java/com/lix/jenkins/JenkinsController.java
@@ -0,0 +1,18 @@
+package com.lix.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: jenkins
+ * @description:
+ * @author: lixin
+ * @create: 2020-05-25 09:43
+ **/
+@RestController
+public class JenkinsController {
+    @GetMapping("jenkinsGet")
+    public String jenkinsGet() {
+        return "Jenkins-success";
+    }
+}
